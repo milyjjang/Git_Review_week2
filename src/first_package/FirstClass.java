bfd16f522baa81a385d6cb24b9dad31a117b72d8
milyjjang <114306483+milyjjang@users.noreply.github.com>
2023-01-07 14:32:23 -0500

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,5 +5,7 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        // print second time
+        System,out.println("First Class");
     }
 }
